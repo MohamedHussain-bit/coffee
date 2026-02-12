4590418a01f5b52420e5b8e576b65bafe7d29f9c
MohamedHussain-bit <godzela.1242006@gmail.com>
2026-02-12 21:24:24 +0200

    add function to catch if not found routes

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const morgan = require('morgan');
 
 const connectDB = require('./config/connectDB');
+const ApiError = require('./utils/apiError');
 
 const PORT = process.env.PORT || 3000;
 
@@ -17,6 +18,11 @@ if(process.env.NODE_ENV === 'development'){
     app.use(morgan('dev'))
     console.log(`mode ${process.env.NODE_ENV}`);
 };
+
+// If not found routes
+app.use((req , res , next) => {
+    next(new ApiError(`can not found this route ${req.originalUrl}` , 404));
+});
 
 // Global error handeling middleware
 app.use((err , req , res , next) => {
