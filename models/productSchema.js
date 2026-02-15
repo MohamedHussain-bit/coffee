e3594f11b08d86b117f8f12ffdb7903e1b11f27c
MohamedHussain-bit <godzela.1242006@gmail.com>
2026-02-15 23:53:23 +0200

    careate product schema

---
+++ models/productSchema.js
@@ -0,0 +1,61 @@
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+    title : {
+        type : String,
+        required : [true , 'title product required'],
+        minLength : [5 , 'too short title product'],
+        maxLength : [20 , 'too long title product'],
+    },
+    slug : {
+        type : String,
+        lowercase : true,
+    },
+    description : {
+        type : String,
+        required : true,
+        minLength : [20 , 'to short descripton in this product']
+    },
+    quantity : {
+        type : Number,
+        required : true,
+        default : 0
+    },
+    sold : {
+        type : Number,
+        default : 0
+    },
+    price : {
+        type : Number,
+        required : true,
+        max : [100000 , 'too long price']
+    },
+    size : {
+        type : String,
+        required : true,
+    },
+    priceAfterDiscount : {
+        type : Number,
+    },
+    imageCover : {
+        type : String,
+        required : true,
+    },
+    images : [String],
+    category : {
+        type : mongoose.Schema.ObjectId,
+        ref : 'Category',
+        required : [true , 'product must be belong to category']
+    },
+    ratingsAverage : {
+        type : Number,
+        min : [1 , 'Rating average must be above or equale 1.0'], 
+        max : [5 , 'Rating average must be below or equale 5.0'] 
+    },
+    ratingsQuantity : {
+        type : Number,
+        default : 0
+    }
+} , {timestamps : true});
+
+module.exports = mongoose.model('Product' , productSchema);
