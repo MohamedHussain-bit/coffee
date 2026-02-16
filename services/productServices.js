b61b36418cedff3979a80da71437bd56c6676a57
MohamedHussain-bit <godzela.1242006@gmail.com>
2026-02-16 13:21:18 +0200

    grate handler to get products

--- services/productServices.js
+++ services/productServices.js
@@ -13,3 +13,23 @@ exports.createProduct = asyncHandler(async (req , res , next) => {
     });
 });
 
+// @desc     Get products
+// @route    GET api/v1/products
+// @access   public
+exports.getAllProducts = asyncHandler(async (req , res , next) => {
+    const page = Number(req.query.page) || 1;
+    const limit = Number(req.query.limit) || 5;
+    const skip = (page - 1) * limit;
+
+    const products = await Product.find({}).skip(skip).limit(limit);
+
+    if(!products){
+        return next(new ApiErorr('Products not found' , 404));
+    };
+
+    return res.status(200).json({
+        results : products.length,
+        page : page,
+        data : products,
+    });
+});
