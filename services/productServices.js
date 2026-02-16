173d9fd5eb4206100efaf24afd479afe7ae33caa
MohamedHussain-bit <godzela.1242006@gmail.com>
2026-02-16 13:29:34 +0200

    create handler to get specific product

--- services/productServices.js
+++ services/productServices.js
@@ -33,3 +33,18 @@ exports.getAllProducts = asyncHandler(async (req , res , next) => {
         data : products,
     });
 });
+
+// @desc     Get specific products
+// @route    GET api/v1/products/:id
+// @access   public
+exports.getSpecificProduct = asyncHandler(async (req , res , next) => {
+    const {id} = req.params;
+
+    const product = await Product.findById(id);
+
+    if(!product){
+        return next(new ApiErorr(`product for this id ${id} not found` , 404));
+    };
+
+    return res.status(200).json({data : product});
+});
