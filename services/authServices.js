799be9f4e566ee891cae63d6bf61aa312500bc61
MohamedHussain-bit <godzela.1242006@gmail.com>
2026-03-09 23:51:41 +0200

    create login

--- services/authServices.js
+++ services/authServices.js
@@ -1,5 +1,6 @@
 const asyncHandler = require('express-async-handler');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt')
 
 const ApiError = require('../utils/apiError');
 const User = require('../models/userSchema');
@@ -21,4 +22,22 @@ exports.signup = asyncHandler(async (req , res , next) => {
         {expiresIn : process.env.JWT_SECRET_TIME}
     );
     return res.status(201).json({data : user , token});
+});
+
+// @desc     Login
+// @route    POST api/v1/auth/signup
+// @access   Public
+exports.login = asyncHandler(async (req , res , next) => {
+    // check if user exist and check if password correct
+    const user = await User.findOne({email : req.body.email});
+    if(!user || !bcrypt.compare(req.body.password , user.password)){
+        return next(new ApiError('Incorect email or password' , 401));
+    };
+    // Generate token
+    const token = jwt.sign(
+        {userId : user._id},
+        process.env.JWT_SECRET_KEY,
+        {expiresIn : process.env.JWT_SECRET_TIME}
+    );
+    return res.status(200).json({data : user , token});
 });
