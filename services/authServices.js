b70167af06238408f7eaf9b5eaa7d21023cbcb16
MohamedHussain-bit <godzela.1242006@gmail.com>
2026-03-09 17:56:50 +0200

    create signup

---
+++ services/authServices.js
@@ -0,0 +1,21 @@
+const asyncHandler = require('express-async-handler');
+const jwt = require('jsonwebtoken');
+
+const ApiError = require('../utils/apiError');
+const User = require('../models/userSchema');
+
+exports.signup = asyncHandler(async (req , res , next) => {
+    // create user
+    const user = await User.create({
+        name : req.body.name,
+        email : req.body.email,
+        password : req.body.password,
+    });
+    // Generate token
+    const token = jwt.sign(
+        {userId : user._id},
+        process.env.JWT_SECRET_KEY,
+        {expiresIn : process.env.JWT_SECRET_TIME}
+    );
+    return res.status(201).json({data : user , token});
+});
