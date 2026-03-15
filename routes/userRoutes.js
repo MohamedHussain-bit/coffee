bf8b9a68d9f1906fb936aa1a12cae18b9cb8fe0b
MohamedHussain-bit <godzela.1242006@gmail.com>
2026-03-15 14:58:41 +0200

    protected this routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -18,7 +18,8 @@ const {
 } = require('../utils/validatorRoles/userValidator');
 
 const {
-    protected
+    protected,
+    allowedTo,
 } = require('../services/authServices');
 
 const router = express.Router();
@@ -28,12 +29,34 @@ router.route('/changePasswored/:id')
     .put(chengeUserPasswordValidator , changePassword)
 
 router.route('/')
-    .post(careateUserValidator , createUser)
-    .get(protected , getAllUser)
+    .post(
+        protected,
+        allowedTo('admin'),
+        careateUserValidator, 
+        createUser
+    )
+    .get(
+        protected, 
+        getAllUser
+    )
 
 router.route('/:id')
-    .get(getUserValidator , getUser)
-    .put(updateUserValidator , updateUser)
-    .delete(deleteUserValidator , deleteUser)
+    .get(
+        protected, 
+        getUserValidator,
+        getUser
+    )
+    .put(
+        protected,
+        allowedTo('admin'),
+        updateUserValidator, 
+        updateUser
+    )
+    .delete(
+        protected,
+        allowedTo('admin'),
+        deleteUserValidator, 
+        deleteUser
+    )
 
 module.exports = router;
