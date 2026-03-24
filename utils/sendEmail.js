c55d4d68d5563308ed555d4e9360078848186267
MohamedHussain-bit <godzela.1242006@gmail.com>
2026-03-24 14:28:18 +0200

    add email option

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,6 +1,6 @@
 const nodemailer = require('nodemailer');
 
-const sendEmail = (options) => {
+const sendEmail = async (options) => {
     const transporter = nodemailer.createTransport({
         host : process.env.EMAIL_HOST,
         port : process.env.EMAIL_PORT,
@@ -10,6 +10,14 @@ const sendEmail = (options) => {
             pass : process.env.EMAIL_PASSWORD
         }
     });
+    // Define email option (from , to , texst)
+    const mailOption = {
+        from : 'Coffee-App <medohussain51@gmail.com>',
+        to : options.email,
+        subject : options.subject,
+        text : options.message
+    };
+    await transporter.sendMail(options);
 };
 
 module.exports = sendEmail;
